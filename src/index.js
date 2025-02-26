14fb050b31dfc38dec8a7d5a721a3743b22e7fe8
GeeksforGeeks <rlmagtira@gmail.com>
2025-02-26 13:45:59 +0800

    Modifications
    // added modifications for testing

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 const router = require('./router');
-const profileRoute = require('./routes/profileRoute'); // <-- Import the new route
+const profileRoute = require('./routes/profileRoute'); // Import the new route
 
 dotenv.config();
 
 const app = express();
 
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,  
     useUnifiedTopology: true,
@@ -18,13 +19,18 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.log('Error in connection:', err));
 
+// Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', // Change to your actual frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(morgan('tiny'));
 
+// Routes
 app.use(router);
-app.use('/profile', profileRoute); // <-- Add the new route here
+app.use('/profile', profileRoute); 
 
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
-});
+// Export for Vercel
+module.exports = app;
