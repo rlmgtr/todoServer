b0d6e4a9fbf3bf58f5402218ec0793a065d5a259
GeeksforGeeks <rlmagtira@gmail.com>
2025-02-26 14:46:24 +0800

    update

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ const updateTodoRoute = require('./routes/updateTodoRoute');
 const deleteTodoRoute = require('./routes/deleteTodoRoute');
 
 router.use('/signup', signUpRoute);
-router.post('/login', loginRoute);
+router.use('/login', loginRoute);
 router.use('/todos', isLoggedIn, createTodoRoute);
 router.use('/todos', isLoggedIn, readTodoRoute);
 router.use('/todos', isLoggedIn, updateTodoRoute);
